122a48cef823dbfe4842a2767618ce23c4a7d7c9
Murad Sarker <sarker24@gmail.com>
2021-01-27 01:47:09 +0100

    Impliment the autocompletf functionality

---
+++ app/javascript/packs/people.js
@@ -0,0 +1,15 @@
+document.addEventListener("turbolinks:load", function() {
+
+    $input = $('*[data-behavior="autocomplete"]')
+  
+    var options = {
+      url: function(phrase) {
+        return "/people/search.json?q=" + phrase;
+      },
+      getValue: "name",
+    };
+  
+    $input.easyAutocomplete(options);
+  
+  });
+  
